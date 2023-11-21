e2f4bc60d037bdf7a843277a31ac15b457bbc585
Dylan <Dylan.letoille@gmail.com>
2023-11-21 11:57:35 +0400

    fixed broken_pirate

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?")  # added double quote at the end of the string
+if greeting in ["Arrr!"]:  # replaced parenthesis with square bracket
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else:  # replaced elif with else, added colon and tabbed the print statement underneath
+	print("Greetings, hater of pirates!")
